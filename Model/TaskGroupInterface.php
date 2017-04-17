66240d990c65b416192f4f4dfdc7ea37f1b51fea
miky <mathias89100@gmail.com>
2017-04-17 03:14:26 +0200

    Merge branch 'master' of github.com:mikyprog/AdminBundle
    # Veuillez entrer un message de validation pour expliquer en quoi cette fusion est
    # nécessaire, surtout si cela fusionne une branche amont mise à jour dans une branche de sujet.
    #
    # Les lignes commençant par '#' seront ignorées, et un message vide
    # abandonne la validation.

---
+++ Model/TaskGroupInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miky
+ * Date: 14/11/16
+ * Time: 23:06
+ */
+
+namespace Miky\Component\Task\Model;
+
+
+interface TaskGroupInterface
+{
+
+}
---
+++ Model/TaskInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miky
+ * Date: 14/11/16
+ * Time: 23:04
+ */
+
+namespace Miky\Component\Task\Model;
+
+
+interface TaskInterface
+{
+
+}
